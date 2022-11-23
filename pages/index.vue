dde15d82810136611c3629f36b5e33ebc1d0694f
zxx <ss15037500392@163.com>
2022-11-23 19:51:21 +0800

    容错修改

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,23 @@
 <template>
-    <div>index</div>
+  <div v-if="pending">加载中。。。</div>
+  <div v-else-if="error">{{ error?.data }}</div>
+  <div v-else>{{ data }}</div>
 </template>
 <script setup>
-    import {
-        NIcon
-    } from "naive-ui"
-    import {
-        Search
-    } from '@vicons/ionicons5'
+import { throwError } from "naive-ui/es/_utils";
+const { data, pending, error } = await useFetch("/index", {
+  key: "IndexData",
+  baseURL: "http://demonuxtapi.dishait.cn/pc",
+  headers: {
+    appid: "bd9d01ecc75dbbaaefce",
+  },
+  transform: (res) => {
+    return res.data;
+  },
+  lazy: true,
+});
+
+if (process.server && error.value) {
+  throwError(error.value?.data?.data);
+}
 </script>
